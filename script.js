d754dfb621bde55a958fbe8d9b4dcdf6a9a3bdb9
usmanbutt-dev <buttu7666@gmail.com>
2024-06-20 04:31:17 +0500

    Printing data on console done

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
+const button = document.querySelector(".weather-button");
+const inputBox = document.querySelector(".city-box");
+
 let api_key = `522bde66bda7a301c461964248b70158`;
 let city = 'lahore';
-let API = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
-fetch(API)
-    .then(function(res) {
-        if(res.ok) {
-            console.log("City Found");
-        }
-        else {
-            console.log("City Not Found");
-        }
-        return res.json();
-    })
-    .then(function(data) {
-        let weather = data.weather[0]
-        let type = weather.main
-        let temperature = data.main.temp;
-        temperature =  temperature - 273.15
-        console.log(type);
-        console.log(temperature);
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
+function fetcher(inputCity) {
+    city = inputCity;
+    let API = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+    fetch(API)
+        .then(function(res) {
+            if(res.ok) {
+                console.log("City Found");
+            }
+            else {
+                console.log("City Not Found");
+            }
+            return res.json();
+        })
+        .then(function(data) {
+            let weather = data.weather[0]
+            let type = weather.main
+            let temperature = data.main.temp;
+            temperature =  temperature - 273.15
+            console.log(type);
+            console.log(temperature);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+}
+
+button.addEventListener(`click`, () => {
+    let inputBoxCity = inputBox.value
+    fetcher(inputBoxCity);
+})
